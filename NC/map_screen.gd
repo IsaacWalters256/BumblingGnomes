a15ce24c02e17437dfd445df5d90435fde9eb202
isaac walters <waltersi@my.lanecc.edu>
2026-09-03 21:26:09 -0700

    start on working overead map

--- NC/map_screen.gd
+++ NC/map_screen.gd
@@ -1,20 +1,20 @@
-extends CanvasLayer
+extends Node2D
 
 #we probably want all this to be control nodes actually for easier movement
 
 var map_tiles = []
 
 func start(map_data):
-	for c in 10:
-		var h_box = HBoxContainer.new()
-		get_node("VBoxContainer").add_child(h_box)
-		map_tiles.append([])
-		for r in 10:
-			var new_tile = load("res://NC/map_tile.tscn").instantiate()
-			h_box.add_child(new_tile)
-			map_tiles[c].append(new_tile)
-		
-	
+	for mpt in get_node("Map0/Tiles").get_children():
+		mpt.start()
+	#for c in 10:
+		#var h_box = HBoxContainer.new()
+		#get_node("VBoxContainer").add_child(h_box)
+		#map_tiles.append([])
+		#for r in 10:
+			#var new_tile = load("res://NC/map_tile.tscn").instantiate()
+			#h_box.add_child(new_tile)
+			#map_tiles[c].append(new_tile)
 
 
 func new_inst():
--- map_tile.gd
+++ map_tile.gd
@@ -1,19 +1,34 @@
 extends TextureButton
 
 #have all this tiles data in here honestly
-@export var id_loc = [0,0]
+
 @export var type = "forest"
 
-func start(type_x,structures=[]):
-	get_node("Hexagon/Button").text = type
-	if type == "city":
-		get_node("Hexagon/Background").modulate = Color(1,1,0.25)
-	elif type == "farm":
-		get_node("Hexagon/Background").modulate = Color(0,0.75,1)
-	elif type == "forest":
-		get_node("Hexagon/Background").modulate = Color(0.05,1,0.25)
+@export var connected1 = TextureButton
+@export var connected2 = TextureButton
+@export var connected3 = TextureButton
+@export var connected4 = TextureButton
+var connected_list = []
+
+
+func start():
+	if "type" in connected1:
+		connected_list.append(connected1)
+	if "type" in connected2:
+		connected_list.append(connected2)
+	if "type" in connected3:
+		connected_list.append(connected3)
+	if "type" in connected4:
+		connected_list.append(connected4)
+	#self.text = type
+	#if type == "city":
+		#get_node("Hexagon/Background").modulate = Color(1,1,0.25)
+	#elif type == "farm":
+		#get_node("Hexagon/Background").modulate = Color(0,0.75,1)
+	#elif type == "forest":
+		#get_node("Hexagon/Background").modulate = Color(0.05,1,0.25)
 
 
 
 func _on_pressed() -> void:
-	get_parent().map_tile_pressed(id_loc)#forget this just bring the ui up ourselfs
+	print(connected_list)#get_parent().map_tile_pressed(id_loc)#forget this just bring the ui up ourselfs
